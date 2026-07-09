65e233054a095f86cfd3b2dff41b71dca61475e5
ProjectBarks <contact@brandonbarker.me>
2026-07-09 14:28:09 -0400

    feat: add graphs/index.ts barrel re-exporting both namespaces

---
+++ graphs/index.ts
@@ -0,0 +1,4 @@
+export { FlowGraph } from './flow-graph/index.js';
+export { Gantt } from './gantt/index.js';
+export type { RenderFlowGraphOptions } from './flow-graph/types.js';
+export type { RenderGanttOptions } from './gantt/types.js';
